7a37264405e90ecfdc13a03dacaa0ca650310b23
Gitsalov Andrey <gicalov.fortech@gmail.com>
2025-01-20 18:06:03 +0300

    feat: some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { SowflakesBlock } from "falling-snowflakes-2";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ISnowflakeOptions } from "./interfaces";
+import { initSnowflakeOptions, inputOptions } from "./constants";
 import "./App.css";
-
-const initSnowflakeOptions = {
-  snowflakesCount: 30,
-  snowflakeSize: 20,
-  branchesCount: 6,
-  rotationSpeed: -1.7,
-  fallingSpeed: 3.7,
-};
 
 const App = () => {
   const [snowflakeOptions, setSnowflakeOptions] =
     useState(initSnowflakeOptions);
+
+  const handleChangeSettings = (fieldName: string, value: number | string) => {
+    setSnowflakeOptions({
+      ...snowflakeOptions,
+      [fieldName]: fieldName === "snowflakesColor" ? value : Number(value),
+    });
+  };
 
   return (
     <Router basename="/-tasks-development">
@@ -21,23 +22,58 @@ const App = () => {
         <Route
           path="/"
           element={
-            <div style={{ height: "800px", width: "100%" }}>
+            <div
+              style={{
+                width: "100%",
+                height: "100vh",
+              }}
+            >
               <SowflakesBlock snowflakeOptions={snowflakeOptions}>
                 <div
                   style={{
-                    display: "flex",
                     margin: "auto",
-
                     border: "solid",
-                    width: "200px",
-                    height: "100px",
+                    width: "240px",
+                    padding: "20px",
+                    boxSizing: "border-box",
+                    maxHeight: "100vh",
+                    overflow: "auto",
                   }}
-                ></div>
+                >
+                  {inputOptions.map((inputBlock, i) => (
+                    <Fragment key={i}>
+                      <p>{`${inputBlock.label} ${
+                        snowflakeOptions[
+                          inputBlock.name as keyof ISnowflakeOptions
+                        ]
+                      }`}</p>
+
+                      <input
+                        style={{ width: "190px" }}
+                        name={inputBlock.name}
+                        type={inputBlock.type}
+                        {...(inputBlock.type === "range" && {
+                          min: inputBlock.min,
+                          max: inputBlock.max,
+                          step: inputBlock.step,
+                        })}
+                        step="1"
+                        value={
+                          snowflakeOptions[
+                            inputBlock.name as keyof ISnowflakeOptions
+                          ]
+                        }
+                        onChange={(e) =>
+                          handleChangeSettings(e.target.name, e.target.value)
+                        }
+                      />
+                    </Fragment>
+                  ))}
+                </div>
               </SowflakesBlock>
             </div>
           }
         />
-        <Route path="*" element={<>пздц</>} />
       </Routes>
     </Router>
   );
---
+++ src/constants.ts
@@ -0,0 +1,62 @@
+export const initSnowflakeOptions = {
+  snowflakeType: 1,
+  snowflakesCount: 30,
+  snowflakeSize: 20,
+  branchesCount: 6,
+  rotationSpeed: 2,
+  fallingSpeed: 1,
+  snowflakesColor: "#00D2FF",
+  isSynchronousRotation: true,
+};
+
+export const inputOptions = [
+  {
+    label: "Скин снежинок:",
+    name: "snowflakeType",
+    type: "range",
+    min: 1,
+    max: 6,
+    step: 1,
+  },
+  {
+    label: "Размер снежинки:",
+    name: "snowflakeSize",
+    type: "range",
+    min: 0,
+    max: 100,
+    step: 1,
+  },
+  {
+    label: "Число ветвей:",
+    name: "branchesCount",
+    type: "range",
+    min: 1,
+    max: 40,
+    step: 1,
+  },
+  {
+    label: "Скорость падения:",
+    name: "fallingSpeed",
+    type: "range",
+    min: -10,
+    max: 20,
+    step: 0.1,
+  },
+  {
+    label: "Скорость вращения:",
+    name: "rotationSpeed",
+    type: "range",
+    min: -10,
+    max: 10,
+    step: 1,
+  },
+  { label: "Цвет снежинки:", name: "snowflakesColor", type: "color" },
+  {
+    label: "Колличество снежинок:",
+    name: "snowflakesCount",
+    type: "range",
+    min: 1,
+    max: 300,
+    step: 10,
+  },
+];
