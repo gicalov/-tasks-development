57441dc780101efd544abf73ddb45cb199eb3074
Gitsalov Andrey <gicalov.fortech@gmail.com>
2025-01-20 12:07:48 +0300

    feat: gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
+import { useState } from "react";
 import { SowflakesBlock } from "falling-snowflakes-2";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+const initSnowflakeOptions = {
+  snowflakesCount: 30,
+  snowflakeSize: 20,
+  branchesCount: 6,
+  rotationSpeed: -1.7,
+  fallingSpeed: 3.7,
+};
+
 const App = () => {
-  const snowflakeOptions = {
-    snowflakesCount: 30,
-    snowflakeSize: 20,
-    branchesCount: 6,
-    rotationSpeed: -1.7,
-    fallingSpeed: 3.7,
-  };
+  const [snowflakeOptions, setSnowflakeOptions] =
+    useState(initSnowflakeOptions);
 
   return (
-    <div style={{ height: "800px", width: "100%" }}>
-      <SowflakesBlock snowflakeOptions={snowflakeOptions}>
-        <div
-          style={{
-            display: "flex",
-            margin: "auto",
-            // marginTop: "600px",
-            backgroundColor: "red",
-            width: "200px",
-            height: "100px",
-          }}
-        ></div>
-      </SowflakesBlock>
-    </div>
+    <Router basename="/-tasks-development">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ height: "800px", width: "100%" }}>
+              <SowflakesBlock snowflakeOptions={snowflakeOptions}>
+                <div
+                  style={{
+                    display: "flex",
+                    margin: "auto",
+
+                    border: "solid",
+                    width: "200px",
+                    height: "100px",
+                  }}
+                ></div>
+              </SowflakesBlock>
+            </div>
+          }
+        />
+        <Route path="*" element={<>пздц</>} />
+      </Routes>
+    </Router>
   );
 };
 
